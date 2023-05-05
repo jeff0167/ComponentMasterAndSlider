102f577dfe52ac856439e08755b8b95206b5af32
jefferen <jeff0167@edu.zealand.dk>
2023-05-05 14:46:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ComponentMasterAndSlider',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
