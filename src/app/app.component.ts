8e386e167223c1f54402d8009f0988d6cf78d554
jefferen <jeff0167@edu.zealand.dk>
2023-05-14 20:45:46 +0200

    Docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule]
 })
 export class AppComponent {
   constructor() {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule, PopoverController } from '@ionic/angular';
+
+import { PopUpComponent } from '../components/pop-up/pop-up.component';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, PopUpComponent]
 })
 export class HomePage {
 
-  constructor() {}
+  MyVal: boolean = false;
+  
+  constructor(public popoverController: PopoverController) {}
+
+  async presentPopover(e: Event) { // still a bunch of code just to use a object/component
+    const popover = await this.popoverController.create({  // we create a popup from the existing pop-upComponent class so to say, you could switch out the component to create
+      component: PopUpComponent,
+      componentProps: { description: "Are you sure about that?" } // value to send as input param
+    });
+
+      await popover.present(); 
+      
+      popover.onDidDismiss()
+      .then((data) => {
+        //const user = data['data']; 
+        this.MyVal = data?.data;
+      });
+  }
 
 }
